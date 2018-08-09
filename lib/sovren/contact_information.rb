10b8930ec24e98ccf7452e2f25ffc237305b5605
Ryan Vall <rvall@patriotsoftware.com>
2018-08-09 10:27:59 -0400

    smarter parsing of phones, return all email addresses

--- lib/sovren/contact_information.rb
+++ lib/sovren/contact_information.rb
@@ -21,13 +21,34 @@ module Sovren
       result.postal_code = contact_information.css('PostalAddress').first.css('PostalCode').text rescue nil
       result.country = contact_information.css('PostalAddress').first.css('CountryCode').text rescue nil
 
-      result.home_phone = contact_information.css('Telephone FormattedNumber').first.text rescue nil
-      result.mobile_phone = contact_information.css('Mobile FormattedNumber').first.text rescue nil
+      result.phone_numbers = []
+      mobile_phones = contact_information.css('Mobile FormattedNumber').map { |m| { type: 'mobile', number: m.text } } rescue nil
+      fax_phones = contact_information.css('Fax FormattedNumber').map { |m| { type: 'fax', number: m.text } } rescue nil
 
-      result.websites = contact_information.css('InternetWebAddress').map { |w| w.text } rescue nil
-      result.email = contact_information.css('InternetEmailAddress').first.text rescue nil
+      other_phone_nodes = contact_information.css('ContactMethod')&.select { |node| node.css('Telephone').present? }
+      home_phones = parse_phones_of_type('home', other_phone_nodes) if other_phone_nodes.present?
+      work_phones = parse_phones_of_type('office', other_phone_nodes) if other_phone_nodes.present?
+
+      result.phone_numbers.concat(mobile_phones) if mobile_phones.present?
+      result.phone_numbers.concat(fax_phones) if fax_phones.present?
+      result.phone_numbers.concat(home_phones) if home_phones.present?
+      result.phone_numbers.concat(work_phones) if work_phones.present?
+
+      result.websites = contact_information.css('InternetWebAddress').map(&:text) rescue nil
+      result.email_addresses = contact_information.css('InternetEmailAddress').map(&:text) rescue nil
 
       result
+    end
+
+    def self.parse_phones_of_type(type, phone_nodes)
+      type_mappings = {
+        home: 'home',
+        office: 'work'
+      }
+      matching_phone_nodes = phone_nodes.select { |node| node.css('Location')&.text == type }
+      number_nodes = matching_phone_nodes.map { |node| node.css('FormattedNumber') }
+      v3_type = type_mappings[type.to_sym]
+      number_nodes.map { |n| { type: v3_type, number: n.text } }
     end
   end
 end
